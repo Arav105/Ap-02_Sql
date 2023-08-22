ac35b27534c4663dd7162be49d907e047eeeb0e2
Arav105 <chachuarav@gmail.com>
2023-08-23 01:50:23 +0530

    Ap-2

---
+++ Ap-2.sql
@@ -0,0 +1,14 @@
+create Database Assissted
+use Assissted
+
+Create Table School
+(Id int Primary Key,
+Name nvarchar(50),
+Class nvarchar(50))
+
+Insert Into School Values
+(1,'Raghul','12B'),
+(2,'Varun','12C'),
+(3,'Arav','12E');
+
+Select * From school
